82188299b7add82134d9cc0e17479294f71ddfff
DucAnhDev <ntducanh9421@gmail.com>
2025-04-06 12:30:37 +0700

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';
+import ProfileUser from './pages/ProfileUser';
+
 
 
 export default function App() {
@@ -15,6 +17,7 @@ export default function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<AuthPage />} />
             <Route path="/register" element={<AuthPage />} />
+            <Route path="/profileuser" element={<ProfileUser />} />          
           </Routes>
         </main>
         <Footer />
